59b3fc2c37bb3cdc958355ba29b4ea3626ed3332
yiyun <sunyiyun.2007@163.com>
2016-01-29 14:56:23 +0800

    Create FormatUtils.java

---
+++ FormatUtils.java
@@ -0,0 +1,20 @@
+package itec.utils;
+
+import java.text.DecimalFormat;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class FormatUtils {
+    
+    static Log log = LogFactory.getLog(FormatUtils.class);
+    
+    public static String readableFileSize(long size) {
+        if (size <= 0) {
+            return "0";
+        }
+        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+}
